59f4ad1de2a6d9b69b87b8a3e9114f5f17fab2a0
Carlos <xxtcarlovisk07@gmail.com>
2022-01-31 18:58:32 -0300

    creating the routes

---
+++ lib/app/routes/splash_routers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/app/modules/splash/splash_page.dart';
+
+class SplashRouters {
+  SplashRouters._();
+
+  static final routers = <GetPage>[
+    GetPage(
+      name: '/',
+      page: () => const SplashPage(),
+    )
+  ];
+}
